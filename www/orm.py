3107cac8eea66401e5aec3a47bf03bd1a808593d
midu945 <sunwenjun@163.com>
2023-09-05 17:50:30 +0800

    add some files

--- www/orm.py
+++ www/orm.py
@@ -14,11 +14,11 @@ async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
     __pool = await aiomysql.create_pool(
-        host=kw.get('host', 'localhost'),
-        port=kw.get('port', 3306),
-        user=kw['user'],
-        password=kw['password'],
-        db=kw['db'],
+        host=kw.get('host', '172.17.40.242'),
+        port=kw.get('port', 3301),
+        user=kw['root'],
+        password=kw['RG2gjw92gwk'],
+        db=kw['test'],
         charset=kw.get('charset', 'utf8'),
         autocommit=kw.get('autocommit', True),
         maxsize=kw.get('maxsize', 10),
