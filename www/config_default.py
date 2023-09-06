74139ad6d6207d2d54b01daeb8f008ffb4f05e81
midu945 <sunwenjun@163.com>
2023-09-06 11:12:28 +0800

    day6

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'Michael Liao'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '172.17.40.242',
+        'port': 3301,
+        'user': 'root',
+        'password': 'RG2gjw92gwk',
+        'db': 'test'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Michael Liao'
+
+configs = {
+    'db': {
+        'host': '172.17.40.242'
+    }
+}
